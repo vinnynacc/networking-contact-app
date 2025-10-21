4f85f65a4898c3b149cf8d9b7c9c3d7fb1eb8d19
vinnynacc <165830495+vinnynacc@users.noreply.github.com>
2025-10-21 17:18:45 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["Poppins","ui-sans-serif","system-ui"] },
+    },
+  },
+  plugins: [],
+};
